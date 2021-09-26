279ede052a81e3dcd42ba4ca2f66bd59be27952a
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-25 23:35:57 -0400

    Add passing test for GetDescription_ReturnsDescription_String() and update all tests for changes to constructor parameters

--- Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -17,7 +17,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Test vendor");
+      Vendor newVendor = new Vendor("Test Vendor", "Test Vendor Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -25,16 +25,28 @@ namespace Bakery.Tests
     public void GetName_ReturnsName_String()
     {
       string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string description = "Test Vendor Description";
+      Vendor newVendor = new Vendor(name, description);
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string name = "Test Vendor";
+      string description = "Test Vendor Description";
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
 
     [TestMethod]
     public void GetId_ReturnsVendorId_Int()
     {
       string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string description = "Test Vendor Description";
+      Vendor newVendor = new Vendor(name, description);
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
@@ -44,8 +56,10 @@ namespace Bakery.Tests
     {
       string name01 = "New Seasons";
       string name02 = "Stumptown";
-      Vendor newVendor1 = new Vendor(name01);
-      Vendor newVendor2 = new Vendor(name02);
+      string description01 = "Local grocery store chain";
+      string description02 = "Local coffee shop chain";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      Vendor newVendor2 = new Vendor(name02, description02);
       List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -56,8 +70,10 @@ namespace Bakery.Tests
     {
       string name01 = "New Seasons";
       string name02 = "Stumptown";
-      Vendor newVendor1 = new Vendor(name01);
-      Vendor newVendor2 = new Vendor(name02);
+      string description01 = "Local grocery store chain";
+      string description02 = "Local coffee shop chain";
+      Vendor newVendor1 = new Vendor(name01, description01);
+      Vendor newVendor2 = new Vendor(name02, description02);
       Vendor result = Vendor.Find(2);
       Assert.AreEqual(newVendor2, result);
     }
@@ -65,11 +81,12 @@ namespace Bakery.Tests
     [TestMethod]
     public void AddOrder_AssociatesOrderWithVendor_OrderList()
     {
-      string description = "6 challahs";
-      Order newOrder = new Order(description);
+      string orderDescription = "6 challahs";
+      Order newOrder = new Order(orderDescription);
       List<Order> newList = new List<Order> { newOrder };
       string name = "New Seasons";
-      Vendor newVendor = new Vendor(name);
+      string vendorDescription = "Local grocery store chain";
+      Vendor newVendor = new Vendor(name, vendorDescription);
       newVendor.AddOrder(newOrder);
       List<Order> result = newVendor.Orders;
       CollectionAssert.AreEqual(newList, result);
