05ac55425ada76b6a36827f998dfb4a6fca0f1c5
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-26 09:42:47 -0400

    Update constructor to add more auto implemented properties

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,13 +4,21 @@ namespace Bakery.Models
 {
   public class Order
   {
+    public string Title { get; set; }
     public string Description { get; set; }
+    public int Price { get; set; }
+    public string DatePlaced { get; set; }
+    public string DeliveryDate { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string description)
+    public Order(string title, string description, int price, string datePlaced, string deliveryDate)
     {
+      Title = title;
       Description = description;
+      Price = price;
+      DatePlaced = datePlaced;
+      DeliveryDate = deliveryDate;
       _instances.Add(this);
       Id = _instances.Count;
     }
@@ -19,7 +27,7 @@ namespace Bakery.Models
     {
       return _instances;
     }
-    
+
     public static void ClearAll()
     {
       _instances.Clear();
